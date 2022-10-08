f66a02931ca1a156df73d5eb8f12f8fd791f43ca
LorrdSan999 <115376427+LorrdSan999@users.noreply.github.com>
2022-10-09 01:22:19 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -9,5 +9,5 @@ class Config(object):
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     SUPPORT = os.environ.get("SUPPORT", "X223D")
     CHANNEL = os.environ.get("CHANNEL", "OYURL")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3360ae8fd7513c11ce591.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/3360ae8fd7513c11ce591.jpg")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/f6ed60c8e59458c8739b7.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/f6ed60c8e59458c8739b7.jpg")
