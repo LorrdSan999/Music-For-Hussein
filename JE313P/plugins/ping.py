d69af6f6545c30328b781525c1877a1d62aa5016
LorrdSan999 <115376427+LorrdSan999@users.noreply.github.com>
2022-10-09 00:53:18 +0300

    Update ping.py

--- JE313P/plugins/ping.py
+++ JE313P/plugins/ping.py
@@ -5,12 +5,12 @@ from telethon import Button, events
 from JE313P import *
 
 IMG = os.environ.get(
-    "PING_PIC", "https://telegra.ph/file/1414f775c59c3b2bc8d95.jpg"
+    "PING_PIC", "https://telegra.ph/file/9a6d3b1a325656c0c7a13.jpg"
 )
 ms = 4
 
 ALIVE = os.environ.get(
-    "ALIVE", "@O_GH0"
+    "ALIVE", "@UUQEO"
 )
 
 CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
@@ -18,5 +18,5 @@ CAPTION = f"**سرعة البنك:** {ms}\n المالك:『{ALIVE}』"
 
 @JE313P.on(events.NewMessage(pattern="^/بنك"))
 async def _(event):
-    UMM = [[Button.url("مطور السورس", "https://t.me/O_GH0")]]
+    UMM = [[Button.url("مطور السورس", "https://t.me/UUQEO")]]
     await JE313P.send_file(event.chat_id, IMG, caption=CAPTION, buttons=UMM)
