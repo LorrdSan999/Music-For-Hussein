c4b9dd82f77503df73e0d56fac4253104086fdbe
LorrdSan999 <115376427+LorrdSan999@users.noreply.github.com>
2022-10-09 00:57:00 +0300

    Update help.py

--- JE313P/plugins/help.py
+++ JE313P/plugins/help.py
@@ -8,7 +8,7 @@ btn =[
     [Button.inline("القفل", data="locks"), Button.inline("اخرى", data="misc")],
     [Button.inline("الرئيسية", data="start")]]
 
-HELP_TEXT = "اهلا بك في قائمة اوامر بوت علوش\n\nاضغط على الازرار من الاسفل:"
+HELP_TEXT = "اهلا بك في قائمة اوامر بوت حسين \n\nاضغط على الازرار من الاسفل:"
 
 
 @JE313P.on(events.NewMessage(pattern="[!?/]الاوامر"))
